d0db4aa6c7de85728a71e88e2e284bd278297e74
ljbroersen <lj.broersen@gmail.com>
2024-08-02 20:35:48 +0900

    trying a different approach for deployment

--- knexfile.ts
+++ knexfile.ts
@@ -2,32 +2,30 @@
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-require("dotenv").config({ path: "./.env.local" });
+require("dotenv").config({ path: "./.env" });
 
-import knex, { Knex } from 'knex';
+import knex, { Knex } from "knex";
 
 interface KnexConfigMap {
   [key: string]: Knex.Config;
 }
 
 const knexConfig: KnexConfigMap = {
-  development: {
-    client: "pg",
-    connection: process.env.DATABASE_URL || {
-      user: process.env.DB_USER,
-      host: "127.0.0.1",
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      port: 5432,
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations",
-    },
+  client: "pg",
+  connection: process.env.DATABASE_URL || {
+    user: process.env.DB_USER,
+    host: "127.0.0.1",
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: 5432,
+  },
+  pool: {
+    min: 2,
+    max: 10,
+  },
+  migrations: {
+    tableName: "knex_migrations",
+    directory: "./migrations",
   },
 };
 
