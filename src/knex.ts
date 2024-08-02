d64af588501877fdab40bce297fd243c211b1d89
Robert Bolgeo <166631820+robertbolgeo@users.noreply.github.com>
2024-08-03 00:44:26 +0900

    feature: adjusting server settings for deployment.

--- src/knex.ts
+++ src/knex.ts
@@ -1,7 +1,7 @@
 import { Knex, knex } from 'knex';
 import { knexConfig } from '../knexfile';
 
-const environment = process.env.NODE_ENV || 'development';
+const environment = process.env.NODE_ENV || 'production';
 const config = knexConfig[environment];
 
 const db = knex(config);
