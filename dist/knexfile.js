4f08ef1906510fadc5e38975cb4cbb7b341a118b
ljbroersen <lj.broersen@gmail.com>
2024-08-02 22:16:23 +0900

    there are router issues

--- dist/knexfile.js
+++ dist/knexfile.js
@@ -6,23 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
  */
 require("dotenv").config({ path: "./.env.local" });
 const knexConfig = {
-  development: {
-    client: "pg",
-    connection: process.env.DATABASE_URL || {
-      user: process.env.DB_USER,
-      host: "127.0.0.1",
-      database: process.env.DB_NAME,
-      password: process.env.DB_PASSWORD,
-      port: 5432,
+    development: {
+        client: "pg",
+        connection: process.env.DATABASE_URL || {
+            user: process.env.DB_USER,
+            host: "127.0.0.1",
+            database: process.env.DB_NAME,
+            password: process.env.DB_PASSWORD,
+            port: 5432,
+        },
+        pool: {
+            min: 2,
+            max: 10,
+        },
+        migrations: {
+            tableName: "knex_migrations",
+            directory: "./migrations",
+        },
     },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-    migrations: {
-      tableName: "knex_migrations",
-      directory: "./migrations",
-    },
-  },
 };
 exports.default = knexConfig;
