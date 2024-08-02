f05279800fe98b30ebe5062fe7069585c702a545
ljbroersen <lj.broersen@gmail.com>
2024-08-02 16:09:42 +0900

    fix: merge conflicts and issues with the pr

---
+++ knexfile.ts
@@ -0,0 +1,33 @@
+// Update with your config settings.
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+require("dotenv").config({ path: "./.env.local" });
+import knex, { Knex } from "knex";
+
+interface KnexConfigMap {
+  [key: string]: Knex.Config;
+}
+
+const knexConfig: KnexConfigMap = {
+  development: {
+    client: "pg",
+    connection: process.env.DATABASE_URL || {
+      user: process.env.DB_USER,
+      host: "127.0.0.1",
+      database: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      port: 5432,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+      directory: "./migrations",
+    },
+  },
+};
+
+export default knexConfig;
