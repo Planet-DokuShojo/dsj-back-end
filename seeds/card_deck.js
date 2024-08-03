e5bc530eca66559b793c6a8622b90975bac9be3e
ljbroersen <lj.broersen@gmail.com>
2024-08-03 12:14:55 +0900

    changed the seed files

--- seeds/card_deck.js
+++ seeds/card_deck.js
@@ -7,18 +7,24 @@ exports.seed = async function (knex) {
   await knex("card_deck").del();
   await knex("card_deck").insert([
     {
+      deck_id: 1,
+      card_id: 1,
       card_front: "Text",
       card_back: "こんにちは",
       created_at: new Date(),
       updated_at: new Date(),
     },
     {
+      deck_id: 1,
+      card_id: 2,
       card_front: "Text",
       card_back: "東京医科大学病院",
       created_at: new Date(),
       updated_at: new Date(),
     },
     {
+      deck_id: 1,
+      card_id: 3,
       card_front: "Text",
       card_back: "西新宿",
       created_at: new Date(),
--- seeds/user_list.js
+++ seeds/user_list.js
@@ -6,8 +6,8 @@ exports.seed = async function (knex) {
   // Deletes ALL existing entries
   await knex("user_list").del();
   await knex("user_list").insert([
-    { email_address: "jesse_is_amazing@cc35.com" },
-    { email_address: "james123@cc35.com" },
-    { email_address: "meowthnyannyan@cc35.com" },
+    { user_id: 1, email_address: "jesse_is_amazing@cc35.com" },
+    { user_id: 2, email_address: "james123@cc35.com" },
+    { user_id: 3, email_address: "meowthnyannyan@cc35.com" },
   ]);
 };
