945e22937609a15519c92c6c3b86c64535071330
Gustavo Minoru <gminetoma@users.noreply.github.com>
2024-08-05 07:01:42 +0000

    Fix: Trying to fix the server.

--- src/knex.ts
+++ src/knex.ts
@@ -1,14 +1,3 @@
-// import knex from "knex";
-// import knexConfig from "../knexfile";
-// import type { Knex } from "knex";
-
-// const environment = process.env.NODE_ENV || "development";
-// const config = knexConfig[environment];
-
-// const db = knex(config);
-
-// export default db;
-
 const config = require('../knexfile');
 const knex = require('knex')(config);
 
